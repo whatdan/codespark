2e6436dbbc892c64012cb1c49e94d7206323c129
Qin Tengfei <devqin@gmail.com>
2014-02-14 20:17:52 +0800

    add some routes

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+//index.html
+exports.index=function(req,res){
+	res.render("index");
+}
+
+exports.upload=function(req,res){
+	res.render("upload");
+}
+
+// exports.index=function(req,res){
+// 	res.render("index");
+// }
+
+// exports.index=function(req,res){
+// 	res.render("index");
+// }
+
+// exports.index=function(req,res){
+// 	res.render("index");
+// }
+
+// exports.index=function(req,res){
+// 	res.render("index");
+// }
