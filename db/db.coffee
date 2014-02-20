741c38d26e4bd04707d31bcd991d7b7a68576158
Qin Tengfei <devqin@gmail.com>
2014-02-20 21:01:02 +0800

    登录验证

--- db/db.coffee
+++ db/db.coffee
@@ -12,3 +12,17 @@ exports.insert = (email,ps) ->
 			    return;
 		return;
 	return;
+
+exports.verifyLogin = (email,ps,callback) ->
+	MongoClient.connect 'mongodb://localhost:27017/codespark', (err,db)->
+		throw err if err
+		db.collection('kids').findOne
+			"email":email
+			(err,result) ->
+				console.log result.email == email and ps == result.password;
+				callback result.email == email and ps == result.password;
+				db.close();
+				return;
+		return;
+	return ;
+
--- db/db.js
+++ db/db.js
@@ -19,4 +19,19 @@
     });
   };
 
+  exports.verifyLogin = function(email, ps, callback) {
+    MongoClient.connect('mongodb://localhost:27017/codespark', function(err, db) {
+      if (err) {
+        throw err;
+      }
+      db.collection('kids').findOne({
+        "email": email
+      }, function(err, result) {
+        console.log(result.email === email && ps === result.password);
+        callback(result.email === email && ps === result.password);
+        db.close();
+      });
+    });
+  };
+
 }).call(this);
