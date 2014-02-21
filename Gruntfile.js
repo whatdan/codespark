74d13f4029a20c3722478f3111ab9aaa5ee5eb78
Qin Tengfei <devqin@gmail.com>
2014-02-21 21:02:39 +0800

    m gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,7 +21,7 @@ module.exports = function(grunt) {
         files: ['**/*.coffee'],
         tasks: ['coffee'],
         options: {
-          spawn: false,
+          
         },
       },
     }
