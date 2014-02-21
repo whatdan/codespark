9443b26d7aca2863313305fd4bb32cdf66f8e299
Qin Tengfei <devqin@gmail.com>
2014-02-21 21:03:12 +0800

    insert obj

--- db/db.coffee
+++ db/db.coffee
@@ -1,16 +1,10 @@
 mongoClient = require("./mongoClient.js").mongoClient;
 
-exports.insert = (email,ps) -> 
+exports.insert = (obj) -> 
 	mongoClient (err,db)->
 		throw err if err
-		db.collection('kids').insert 
-			"email":email
-			"password":ps,
-			(err,result) ->
-			    db.close();
-			    return;
-		return;
-	return;
+		db.collection('kids').insert obj,(err,result) ->
+			db.close();
 
 exports.verifyLogin = (email,ps,callback) ->
 	mongoClient (err,db)->
@@ -18,7 +12,8 @@ exports.verifyLogin = (email,ps,callback) ->
 		db.collection('kids').findOne
 			"email":email
 			(err,result) ->
-				callback result.email == email and ps == result.password,result._id.toString();
+				console.log result.nickname;
+				callback result.email == email and ps == result.password,result._id.toString(),result.nickname;
 				db.close();
 				return;
 		return;
--- db/db.js
+++ db/db.js
@@ -3,16 +3,13 @@
 
   mongoClient = require("./mongoClient.js").mongoClient;
 
-  exports.insert = function(email, ps) {
-    mongoClient(function(err, db) {
+  exports.insert = function(obj) {
+    return mongoClient(function(err, db) {
       if (err) {
         throw err;
       }
-      db.collection('kids').insert({
-        "email": email,
-        "password": ps
-      }, function(err, result) {
-        db.close();
+      return db.collection('kids').insert(obj, function(err, result) {
+        return db.close();
       });
     });
   };
@@ -25,7 +22,8 @@
       db.collection('kids').findOne({
         "email": email
       }, function(err, result) {
-        callback(result.email === email && ps === result.password, result._id.toString());
+        console.log(result.nickname);
+        callback(result.email === email && ps === result.password, result._id.toString(), result.nickname);
         db.close();
       });
     });
