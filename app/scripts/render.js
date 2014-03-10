dd1a1c1d73d8b0f327216021534e1a98df16592c
devqin <devqin@gmail.com>
2014-03-10 21:48:10 +0800

    render code

--- app/scripts/render.js
+++ app/scripts/render.js
@@ -1,10 +1,10 @@
-(function($){
+// (function($){
 
-var ld = setTimeout(function(){
-	if($("pre.code")!=="{{view}}"){
-		$("pre.code").snippet("javascript",{style:"random",showNum:true});
-		clearTimeout(ld);
-	}
-},100);
+// var ld = setTimeout(function(){
+// 	if($("pre.code")!=="{{view}}" && $("pre.code")!=="" && $("pre.code")!=="{{}}"){
+// 		$("pre.code").snippet("javascript",{style:"random",showNum:true});
+// 		clearTimeout(ld);
+// 	}
+// },100);
 
-}(jQuery))
+// }(jQuery))
